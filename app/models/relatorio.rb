b2d23709523248327cf574cf1892254af213d9c0
sdik <sdiksoftwares@gmail.com>
2017-06-16 10:09:55 -0300

    Update relatorio.rb

--- app/models/relatorio.rb
+++ app/models/relatorio.rb
@@ -2,6 +2,9 @@ class Relatorio < ActiveRecord::Base
   belongs_to :departamento
   belongs_to :setor
   has_many :fishbones, dependent: :destroy
+   
+  validates :departamento, presence: true
+  validates :setor, presence: true
   
   accepts_nested_attributes_for :fishbones
   
